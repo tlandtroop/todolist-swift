b81ec5d54e3f683a2441caa359c8b5166af18d7d
Tyler Landtroop <tlandtroop@ufl.edu>
2023-11-26 12:57:20 -0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Tyler Landtroop on 11/26/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
